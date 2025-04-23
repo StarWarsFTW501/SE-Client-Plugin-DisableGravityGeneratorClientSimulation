d88cc2d9f7411752a601cd90b1be5d31a0d75eeb
Šimon Bartoň <barton.simon@icloud.com>
2025-04-23 23:59:15 +0200

    Plugin initial setup

--- ClientPlugin/Properties/AssemblyInfo.cs
+++ ClientPlugin/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClientPluginTemplate")]
+[assembly: AssemblyTitle("DisableGravityGeneratorClientSim")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClientPluginTemplate")]
+[assembly: AssemblyProduct("DisableGravityGeneratorClientSim")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("A061FC6C-713E-42CD-B413-151AC8A5074C")]
+[assembly: Guid("E5154AD1-BE87-43E5-BF3B-A7CCD608C334")]
 
 // Version information for an assembly consists of the following four values:
 //
